05279b2ae12a4aef91eee947a678090dbe29bcb9
Parker Donat <parkerdonat@gmail.com>
2015-04-28 17:18:01 -0600

    Score functionality added

--- ScoreViewController.m
+++ ScoreViewController.m
@@ -8,8 +8,12 @@
 
 #import "ScoreViewController.h"
 
-@interface ScoreViewController ()
+static CGFloat scoreViewHeight = 90;
+
+@interface ScoreViewController () <UITextFieldDelegate>
 @property (nonatomic, strong) NSMutableArray *scoreLabels;
+@property UIScrollView *scrollView;
+
 @end
 
 @implementation ScoreViewController
@@ -19,8 +23,15 @@
     self.scoreLabels = [[NSMutableArray alloc] init];
     
     self.title = @"Score Keeper";
+    
     UIScrollView *newScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
-    newScrollView.backgroundColor = [UIColor grayColor];
+    newScrollView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:newScrollView];
+    self.scrollView = newScrollView;
+    
+    for (int i=0; i<4; i++) {
+        [self addScoreView:i];
+    }
     
     /*UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(self.view.frame.size.width / 2, 0, 200, 200)];
     title.text = @"Score Keeper";
@@ -30,43 +41,52 @@
      
     [newScrollView setCenter:CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2)];
     [newScrollView addSubview:title];*/
-    [self.view addSubview:newScrollView];
     
 }
 - (void) addScoreView:(int)index
 {
-    CGFloat height = self.view.frame.size.height;
+    //CGFloat height = self.view.frame.size.height;
     CGFloat width = self.view.frame.size.width;
     
-    UILabel *view = [[UILabel alloc] initWithFrame:CGRectMake(self.view.frame.size.width / 2, 0, width, height)];
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(20, index * scoreViewHeight, width, scoreViewHeight)];
     
-    UITextField *name = [[UITextField alloc] init];
-    CGRect nameFrame = name.frame;
-    nameFrame.origin = CGPointMake(10, 50);
-    name.frame = nameFrame;
+    UITextField *name = [[UITextField alloc] initWithFrame:CGRectMake(10, 30, 90, 30)];
+    name.delegate = self;
+    name.tag = -1000;
     name.placeholder = @"Name";
     [view addSubview:name];
     
-    UILabel *score = [[UILabel alloc] init];
-    CGRect scoreFrame = score.frame;
-    scoreFrame.origin = CGPointMake(100, 50);
-    score.frame = scoreFrame;
+    UILabel *score = [[UILabel alloc] initWithFrame:CGRectMake(100, 30, 40, 30)];
+    score.backgroundColor = [UIColor whiteColor];
     [view addSubview:score];
+    
 
     [self.scoreLabels addObject:score];
     
-    UIStepper *minusPlus = [[UIStepper alloc] init];//WithFrame:CGRectMake(300, 20, 40, 40)];
-    CGRect minusPlusFrame = minusPlus.frame;
+    UIStepper *minusPlus = [[UIStepper alloc] initWithFrame:CGRectMake(200, 30, 40, 40)];
+    /*CGRect minusPlusFrame = minusPlus.frame;
     minusPlusFrame.origin = CGPointMake(200, 50);
-    minusPlus.frame = minusPlusFrame;
-    minusPlus.minimumValue = -10;
+    minusPlus.frame = minusPlusFrame; */
+    minusPlus.minimumValue = -100;
     minusPlus.maximumValue = 100;
     [view addSubview:minusPlus];
     minusPlus.tag = index;
+    [minusPlus addTarget:self action:@selector(buttonTapped:) forControlEvents:UIControlEventValueChanged];
     
-    [self.view addSubview:view];
+    [self.scrollView addSubview:view];
     
     //score ++ scoreLabels array everytime index changes
+    
+}
+
+- (void)buttonTapped:(UIStepper *)sender{
+
+    UIStepper *stepper = sender;
+    NSInteger index = stepper.tag;
+    int value = [stepper value];
+    
+    UILabel *scoreLabel = self.scoreLabels[index];
+    scoreLabel.text = [NSString stringWithFormat:@"%d", value];
     
 }
 - (void)didReceiveMemoryWarning {
